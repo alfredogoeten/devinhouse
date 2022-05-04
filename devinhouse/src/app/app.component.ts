e4eb5f828169cbffdc057c37dc3b10c19d32a881
Alfredo Goeten <alfredo.goeten@audaces.com>
2022-05-04 18:22:01 -0300

    initial commit

---
+++ devinhouse/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public name = 'DEVinhouse'
+  public time = new Date();
+  private intervalId: any;
+  private clock: any;
+
+
+  ngOnInit() {
+    this.updateClock();
+    this.clock = document.getElementById("clock");
+  }
+
+  public updateClock() {
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+      this.checkTime();
+    }, 1000);
+  }
+
+  
+
+  public checkTime(): void {
+    if (this.time.getSeconds() % 2) {
+      this.clock.style.color = 'red';
+    }
+    else if (!(this.time.getSeconds() % 2)) {
+      this.clock.style.color = 'black';
+    }
+  }
+
+  
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+}
+
