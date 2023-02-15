bfeedb34dca94ee622b69e5348ece678fc242991
Alfredo Goeten <alfredo.goeten@audaces.com>
2023-02-15 16:37:09 -0300

    update 2023

--- devinhouse/src/app/app.component.ts
+++ devinhouse/src/app/app.component.ts
@@ -1,46 +1,37 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public pageName = 'DEVinhouse'
+  public pageName = 'DEVinhouse';
   public time = new Date();
   private intervalId: any;
   private clock: any;
 
-
-  ngOnInit() {
+  public ngOnInit(): void {
     this.updateClock();
-    this.clock = document.getElementById("clock");
+    this.clock = document.getElementById('clock');
   }
 
-  public updateClock() {
+  public ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  private updateClock(): void {
     this.intervalId = setInterval(() => {
       this.time = new Date();
       this.checkTime();
     }, 1000);
   }
 
-  
-
-  public checkTime(): void {
+  private checkTime(): void {
     if (this.time.getSeconds() % 2) {
       this.clock.style.color = 'red';
-    }
-    else if (!(this.time.getSeconds() % 2)) {
+    } else if (!(this.time.getSeconds() % 2)) {
       this.clock.style.color = 'black';
     }
   }
-
-  
-
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
-  }
-
 }
-
